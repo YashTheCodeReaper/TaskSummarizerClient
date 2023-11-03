73ee370540432f527d9c268115355da63e7feb73
Yashwanthkumar Arivazhagan <yashwanthkumar.arivazhagan@tetherfi.com>
2023-11-03 20:25:09 +0530

    dashboard

--- src/app/main/dashboard/dashboard.component.ts
+++ src/app/main/dashboard/dashboard.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  ngOnInit(): void {
+    const timeFlexEl: any = document.querySelector('.time-block-flex');
+    const taskSheetEl: any = document.querySelector('.task-sheet');
 
+    timeFlexEl.addEventListener('scroll', function () {
+      taskSheetEl.scrollTop = timeFlexEl.scrollTop;
+    });
+
+    taskSheetEl.addEventListener('scroll', function () {
+      timeFlexEl.scrollTop = taskSheetEl.scrollTop;
+    });
+  }
 }
