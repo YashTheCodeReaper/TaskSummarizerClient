c8a7fd3cfc41311ab363ca1216a42415cfea52e5
Poornachandra M <poornachandra.m@tetherfi.com>
2023-11-07 08:42:18 +0530

    :sparkles: feat(Route): Added routes for auth component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/Login', pathMatch: 'full' },
+  { path: 'Login', component: AuthorizationComponent },
+  { path: 'dashboard', component: MainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
