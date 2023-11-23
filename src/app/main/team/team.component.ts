c7781a10b820cb951052c2d1298f24202b7dc7f0
Yashwanthkumar Arivazhagan <yashwanthkumar.arivazhagan@tetherfi.com>
2023-11-23 19:39:06 +0530

    teams 2

--- src/app/main/team/team.component.ts
+++ src/app/main/team/team.component.ts
@@ -13,6 +13,146 @@ export class TeamComponent {
     loop: true,
     autoplay: true,
   };
+  members = [
+    {
+      name: 'Suraj Kumar',
+      designation: 'Technical Lead',
+      profilePic: 'https://i.pravatar.cc/100?img=52',
+      jiraHeat: [
+        {
+          name: 'Product QA',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+        {
+          name: 'Engineering QA',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+        {
+          name: 'Project QA',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+        {
+          name: 'Product Backlogs',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+      ],
+    },
+    {
+      name: 'Chirag Ramdas',
+      designation: 'Senior Software Engineer',
+      profilePic: 'https://i.pravatar.cc/100?img=53',
+      jiraHeat: [
+        {
+          name: 'Product QA',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+        {
+          name: 'Engineering QA',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+        {
+          name: 'Project QA',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+        {
+          name: 'Product Backlogs',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+      ],
+    },
+    {
+      name: 'Poornachandra M',
+      designation: 'Software Engineer',
+      profilePic: 'https://i.pravatar.cc/100?img=42',
+      jiraHeat: [
+        {
+          name: 'Product QA',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+        {
+          name: 'Engineering QA',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+        {
+          name: 'Project QA',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+        {
+          name: 'Product Backlogs',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+      ],
+    },
+    {
+      name: 'Kavana K',
+      designation: 'Software Engineer',
+      profilePic: 'https://i.pravatar.cc/100?img=59',
+      jiraHeat: [
+        {
+          name: 'Product QA',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+        {
+          name: 'Engineering QA',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+        {
+          name: 'Project QA',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+        {
+          name: 'Product Backlogs',
+          heatMap: Array.from({ length: 7 }, () => this.getRandomInt(0, 5)),
+        },
+      ],
+    },
+  ];
 
-  constructor(public appControlService: AppControlService) {}
+  constructor(public appControlService: AppControlService) {
+    this.prepareHeatMap();
+  }
+
+  getColor(projectName: string): string | any {
+    try {
+      switch (projectName) {
+        case 'Product QA':
+          return '#7da9ff';
+        case 'Engineering QA':
+          return '#f48c7d';
+        case 'Project QA':
+          return '#ffe27c';
+        default:
+          return '#ff7edb';
+      }
+    } catch (error) {
+      console.error();
+    }
+  }
+
+  prepareHeatMap(): void {
+    try {
+      this.members.forEach((memberObj: any) => {
+        let highestCount: number = 0;
+        memberObj.jiraHeat.forEach((heatObj: any) => {
+          heatObj.heatMap.forEach((heatCount: any) => {
+            if (heatCount > highestCount) highestCount = heatCount;
+          });
+        });
+
+        memberObj.jiraHeat.forEach((heatObj: any, index: number) => {
+          memberObj.jiraHeat[index].heatMap = heatObj.heatMap.map(
+            (heatCount: any) => {
+              return heatCount / highestCount;
+            }
+          );
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  getRandomInt(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
