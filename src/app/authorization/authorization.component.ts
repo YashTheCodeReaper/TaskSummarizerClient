098dc9f86510ae81784934ba43c777e26faa2e2c
Yashwanthkumar Arivazhagan <yashwanthkumar.arivazhagan@tetherfi.com>
2023-11-27 07:27:22 +0530

    auth 2

--- src/app/authorization/authorization.component.ts
+++ src/app/authorization/authorization.component.ts
@@ -15,23 +15,43 @@ import { AnimationOptions } from 'ngx-lottie';
 })
 export class AuthorizationComponent implements OnInit {
   registerFormGroup!: FormGroup;
-  isSignIn: boolean = true;
+  jiraFormGroup!: FormGroup;
+  isSignIn: boolean = false;
   currentStage: number = 1;
-  authAnimation: AnimationOptions = {
+  authAnimation1: AnimationOptions = {
     path: 'assets/images/main/auth/auth1.json',
     loop: true,
     autoplay: true,
   };
+  authAnimation2: AnimationOptions = {
+    path: 'assets/images/main/auth/auth2.json',
+    loop: true,
+    autoplay: true,
+  };
+  staySigned: boolean = true;
 
   constructor(
     private formBuilder: FormBuilder,
     private apiService: ApiService
   ) {
     this.registerFormGroup = this.formBuilder.group({
-      email: new FormControl('', Validators.required),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+      ]),
       password: new FormControl('', [
         Validators.required,
         Validators.minLength(8),
+      ]),
+    });
+    this.jiraFormGroup = this.formBuilder.group({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+      ]),
+      apiToken: new FormControl('', [
+        Validators.required,
+        Validators.minLength(20),
       ]),
     });
   }
@@ -55,5 +75,39 @@ export class AuthorizationComponent implements OnInit {
       (!formGroup.controls[fieldName].pristine &&
         formGroup.controls[fieldName].invalid)
     );
+  }
+
+  moveForm(): void {
+    try {
+      if (this.currentStage < 3) {
+        if (this.currentStage == 1)
+          this.jiraFormGroup.controls['email'].setValue(
+            this.registerFormGroup.value.email
+          );
+        this.currentStage++;
+      } else {
+        let finalObj = this.registerFormGroup.value;
+        finalObj.jira = this.jiraFormGroup.value;
+        finalObj.staySigned = this.staySigned;
+        console.log(finalObj);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  getButtonValidation(): boolean | any {
+    try {
+      switch (this.currentStage) {
+        case 1:
+          return this.registerFormGroup.valid;
+        case 2:
+          return this.jiraFormGroup.valid;
+        default:
+          return true;
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
